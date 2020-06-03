6486a5afe76c1f168f0a194bc1fdbd14e108c60f
cadus <caduswd@gmail.com>
2020-06-03 10:00:47 -0300

    transactions - trx com knex

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -32,7 +32,9 @@ routes.post('/points', async (request, response) => {
         items
     } = request.body;
 
-    await knex('points').insert({
+    const trx = await knex.transaction()
+
+    const insertedIds = await trx('points').insert({
         image: 'image-fake',
         name,
         email,
@@ -42,6 +44,17 @@ routes.post('/points', async (request, response) => {
         city,
         uf
     })
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        }
+    })
+
+    await trx('point_items').insert(pointItems);
 
     return response.json({ success: true })
 })
