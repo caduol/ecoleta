d10dd8143376371132f8a57f89a08ec2bbd39a47
cadus <caduswd@gmail.com>
2020-06-04 05:05:26 -0300

    router and form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// JSX: Sintaxe de XML dentro do Javascript
+
+import Routes from './router'
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hellow World!! Eu s2 React</h1>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
