00b9473fb6ae95837873fc0aac41891a5a674388
cadus <caduswd@gmail.com>
2020-06-04 01:08:36 -0300

    refactoring

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,63 +1,28 @@
 import express from 'express'
-import knex from './database/connection';
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router();
 
+const pointsController = new PointsController()
 
-routes.get('/items', async (request, response) => {
+const itemsController = new ItemsController();
 
-    const items = await knex('items').select('*');
+// padrao de metodos da comunidade
+// index - listagem
+// show - exibe um unico registro
+// create - cria o item
+// update - atualiza
+// delete - remove
 
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http:localhost:3333/uploads/${item.image}`
-        }
-    })
+routes.get('/items', itemsController.index)
 
-    return response.json(serializedItems)
-})
-
-
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    const trx = await knex.transaction()
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    })
-
-    const point_id = insertedIds[0]
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id
-        }
-    })
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({ success: true })
-})
+routes.post('/points', pointsController.create)
+routes.get('/points/', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
 
 export default routes;
+
+// Service Pattern
+// Repository Pattern
