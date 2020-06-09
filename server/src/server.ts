34953b7d2af604ad5aab5e30a544b91892d5b685
caduol <eduardo.w3c@gmail.com>
2020-06-09 03:24:16 -0300

    fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import routes from './routes'
 import cors from 'cors'
+import { errors } from 'celebrate'
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(routes)
 
 // servindo arquivos estaticos, podendo ser - imagens, pdf
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333)
